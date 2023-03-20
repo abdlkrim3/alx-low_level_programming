0fde16c58133613442b841fa1b7c5bb0917d0f89
abdlkrim3 <abdlkrim.agoujil@gmail.com>
2023-03-20 13:50:21 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - this is the main function
+ *
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
