14d75bf8c992af7a8147d57b95253f58104e1752
abdlkrim3 <abdlkrim.agoujil@gmail.com>
2023-03-20 14:29:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - this is the main function
+ * main - A c program that prists a line to the standard error
  *
  * Return 1 (Success)
  */
 
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, quote, sizeof(quote) - 1);
 return (1);
 }
