65c629573a19d28affe390d9968d070d0af69449
abdlkrim3 <abdlkrim.agoujil@gmail.com>
2023-03-20 13:58:56 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - this is the main function
  *
- * Return: 1 (Success)
+ * Return 1 (Success)
  */
 
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
