8a91fa53a6d5da98bbe9560702907bc553855d16
abdlkrim3 <abdlkrim.agoujil@gmail.com>
2023-03-20 14:34:19 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,5 +10,6 @@ int main(void)
 {
 char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, quote, sizeof(quote) - 1);
+// Return failure status (nonzero)
 return (1);
 }
